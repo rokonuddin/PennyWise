f4f2476c1620bfb857927417efc677ad32cdae34
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2017-10-12 17:50:05 +0600

    Work in progress

---
+++ PennyWise/Budget/BudgetCell.swift
@@ -0,0 +1,24 @@
+//
+//  BudgetCell.swift
+//  PennyWise
+//
+//  Created by Mohammed Rokon Uddin on 12/10/17.
+//  Copyright © 2017 Monstar Lab Bangladesh Ltd. All rights reserved.
+//
+
+import UIKit
+
+class BudgetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PennyWise/Budget/BudgetView.swift
@@ -0,0 +1,21 @@
+//
+//  BudgetView.swift
+//  PennyWise
+//
+//  Created by Mohammed Rokon Uddin on 12/10/17.
+//  Copyright © 2017 Monstar Lab Bangladesh Ltd. All rights reserved.
+//
+
+import UIKit
+
+class BudgetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
